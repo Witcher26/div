d0b7ea0ca5a4dbfd7fe5d7e436a72305642702d7
Witcher26 <extreme8igor088@mail.ru>
2023-01-17 21:59:35 +0300

    Доступ к div через forEach()

---
+++ index.js
@@ -0,0 +1,11 @@
+const progressNode = document.querySelectorAll('.container');
+
+console.log(progressNode);
+
+progressNode.forEach((el) => {
+    el.onclick =() => {
+        console.log("click");
+        el.style.background = "green";
+        el.style.width = "100px";
+    }
+})
